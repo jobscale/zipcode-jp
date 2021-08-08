8312dc83708ca3f88ae71ef2a4745d838e9e126a
jobscale <jobscale@users.noreply.github.com>
2021-08-09 07:33:48 +0900

    validation updated text field required

--- app/validations/apiValidation.js
+++ app/validations/apiValidation.js
@@ -4,7 +4,7 @@ class ApiValidation {
   slack(req, res, next) {
     const { body } = req;
     const { error } = Joi.object({
-      text: Joi.string().min(1).max(2 ** 16 - 1),
+      text: Joi.string().required().min(1).max(2 ** 16 - 1),
       icon_emoji: Joi.string(),
       username: Joi.string(),
       attachments: Joi.any(),
