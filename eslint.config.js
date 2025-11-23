ba65d8a1f0e86eb08d609c3671ec97eb33e21944
jobscale <jobscale@users.noreply.github.com>
2025-11-24 06:42:17 +0900

    github/workflow updated

--- eslint.config.js
+++ eslint.config.js
@@ -2,36 +2,35 @@ import globals from 'globals';
 import standard from '@jobscale/eslint-plugin-standard';
 import jestPlugin from 'eslint-plugin-jest';
 
-export default [
-  {
-    ...standard.configs.standard,
-    name: 'standard base rule',
-    ignores: ['**/coverage/**', '**/assets/**', '**/*.min.js'],
-    rules: {
-      ...standard.configs.standard.rules,
-      'no-param-reassign': 'off',
-    },
-  }, {
-    name: 'jest rule',
-    files: ['**/*.test.js', '**/__tests__/**/*.js'],
-    languageOptions: {
-      sourceType: 'module',
-      ecmaVersion: 'latest',
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-    plugins: {
-      jest: jestPlugin,
-    },
-    rules: {
-      ...jestPlugin.configs.recommended.rules,
-      'jest/no-disabled-tests': 'warn',
-      'jest/no-focused-tests': 'error',
-      'jest/no-identical-title': 'error',
-      'jest/prefer-to-have-length': 'warn',
-      'jest/valid-expect': 'error',
+export default [{
+  ignores: ['**/coverage/**', '**/assets/**', '**/*.min.js'],
+}, {
+  ...standard.configs.standard,
+  name: 'standard base rule',
+  rules: {
+    ...standard.configs.standard.rules,
+    'no-param-reassign': 'off',
+  },
+}, {
+  name: 'jest rule',
+  files: ['**/*.test.js', '**/__tests__/**/*.js'],
+  languageOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    globals: {
+      ...globals.node,
+      ...globals.jest,
     },
   },
-];
+  plugins: {
+    jest: jestPlugin,
+  },
+  rules: {
+    ...jestPlugin.configs.recommended.rules,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+}];
